b5fdc0fa69b9236998d5d4e4009a6bed44c63a4f
paulo renato borges <pauloborgeszambon@gmail.com>
2024-10-30 22:08:13 -0300

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderAppComponent } from "./header-app/header-app.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderAppComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
