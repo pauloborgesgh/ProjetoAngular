915e8e9e3114cbefb19d64ce20a18491bbe8f790
Paulo.devak <paulo.devakmg@gmail.com>
2024-11-07 20:29:01 -0300

    Revert "Merge commit '08f440beac58e02cfd75783626908cb2c040af61' into HEAD"
    This reverts commit 8a47b8bea94f44a9c5a1aff818e5735995bb2cf2, reversing
    changes made to 9a57d66e0a6a3551b8f58cb158019cc6262aaab6.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,3 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {PagesComponent} from './pages/pages.component';
-import { AboutComponent } from './about/about.component';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', component: PagesComponent }, // Rota para a página inicial
-  { path: 'about', component: AboutComponent } // Rota para a página "Sobre"
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routes: Routes = [];
--- src/app/header-app/header-app.component.ts
+++ src/app/header-app/header-app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FooterAppComponent } from "../footer-app/footer-app.component";
 
-
 @Component({
   selector: 'app-header-app',
   standalone: true,
